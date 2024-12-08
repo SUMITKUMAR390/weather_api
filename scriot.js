09670ec5090b337be87f51f3d028a3901f4c8a7b
SUMITKUMAR390 <sumitkumar23092003@gmail.com>
2024-12-08 22:33:03 +0530

    added to github

--- scriot.js
+++ scriot.js
@@ -12,12 +12,13 @@ const getweather = async(city) =>{
     if(respone.status==404)
     {
         document.querySelector(".error").style.display="block";
-        document.querySelector(".weather").style.display = "nono";
+        document.querySelector(".weather").style.display = "none";
    
     }
 
     else{
       let data = await respone.json();
+      console.log(data)
    document.querySelector(".city").innerHTML = data.name ;
    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
